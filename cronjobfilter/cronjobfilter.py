0d72c42aad2b16052bb9aeab7161f0f48446d484
filopd <blackbeastfilo@live.com>
2020-08-04 01:53:12 +0530

    create a basic function for demonstration purposes.

---
+++ cronjobfilter/cronjobfilter.py
@@ -0,0 +1,16 @@
+"""
+will help to filter list of cronjobs which are coming under from date and to date.
+"""
+
+def greet_user(ip_user_name):
+    """
+    This function will greet the user by printing the line.
+    parameters:
+        ip_user_name (string) : person's name
+    return:
+        None
+    """
+    print(f"Hello my friend {ip_user_name} ! How are you ?")
+
+if __name__ == "__main__":
+    greet_user("Priyesh Naik")
